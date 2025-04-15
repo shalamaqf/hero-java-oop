0acc92f9c43faa73c1ce48721e40788052f25c40
Shalama Fadilla <shalamafadillaa@gmail.com>
2025-04-15 11:49:59 +0700

    add heroType attribute, a constructor, and override display function in HeroStrength.java (subclass). Make new hero object with strength type in Main.java

--- HeroStrength.java
+++ HeroStrength.java
@@ -1,3 +1,14 @@
-public class HeroStrength {
+public class HeroStrength extends Hero{
+    // attribute
+    private String heroType = "Strength";
     
+    // constructor
+    HeroStrength(String name, double health){
+        super(name, health);
+    }
+
+    public void display(){
+        super.display();
+        System.out.println("Type : " + this.heroType + "\n");
+    }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main{
     public static void main(String[] args) {
-        System.out.println("test");
+        // make new object
+        Hero hero1 = new HeroStrength("Iron man", 100);
+        hero1.display();
     }
 }
