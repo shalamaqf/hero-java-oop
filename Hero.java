b542450ec94b8595f1c740e8b00e1cf25d9f03ae
Shalama Fadilla <shalamafadillaa@gmail.com>
2025-04-15 11:40:40 +0700

    add name and health as attribute, a constructor, and a display function

--- Hero.java
+++ Hero.java
@@ -1,3 +1,16 @@
-public class Hero {
-    
+public abstract class Hero {
+    // attribute
+    private String name;
+    private double health;
+
+    // constructor
+    Hero(String name, double health){
+        this.name = name;
+        this.health = health;
+    }
+
+    public void display(){
+        System.out.println("Name : " + this.name);
+        System.out.println("Health : " + this.health);
+    }
 }
